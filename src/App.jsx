26c99f6e193b5eedbbaf2b9bc872d752f4eee1a6
priyankagour04 <priyankag.bba2022@ssism.org>
2024-12-25 16:33:49 +0530

    Basic folder structure and side bar UI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sidebar from './components/sidebar/Sidebar'
+
+function App() {
+
+
+  return (
+    <>
+     <Sidebar/>
+    </>
+  )
+}
+
+export default App
